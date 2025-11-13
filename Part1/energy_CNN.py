f58e746a53a23b55f3fe72edd8d28085c7b7b15d
Jonah <jonah.mougoue@gmail.com>
2025-11-12 19:59:28 -0500

    Move energy_CNN into own file

---
+++ Part1/energy_CNN.py
@@ -0,0 +1,46 @@
+import math
+import torch
+from torch import nn
+from torch import Tensor
+
+class EnergyCNN(nn.Module):
+    def __init__(self, cnn_channels:tuple,hidden_layers:tuple=()):
+        """
+        Model for predicting energy of a wavefunction using a potential map
+        :param cnn_channels: Tuple of tuples containing parameters for each CNN layer
+        :param hidden_layers: Tuple containing ints describing output for each hidden layer
+        """
+        super().__init__()
+
+        cnn_layers = []
+        if cnn_channels:
+            cnn_layers.append(nn.Conv2d(1,cnn_channels[0][0],cnn_channels[0][1],stride=cnn_channels[0][2],padding=cnn_channels[0][3], padding_mode='replicate'))
+            cnn_layers.append(nn.ReLU())
+            for i in range(len(cnn_channels)-1):
+                cnn_layers.append(nn.Conv2d(cnn_channels[i][0],cnn_channels[i+1][0],cnn_channels[i+1][1],stride=cnn_channels[i+1][2],padding=cnn_channels[i+1][3], padding_mode='replicate'))
+                cnn_layers.append(nn.ReLU())
+
+        self.cnn_stack = nn.Sequential(*cnn_layers)
+        cnn_output = self.cnn_stack(torch.zeros(1,256,256))
+
+        linear_layers = []
+        if hidden_layers:
+            linear_layers.append(nn.Flatten())
+            linear_layers.append(nn.Linear(cnn_output.numel(),hidden_layers[0]))
+            linear_layers.append(nn.ReLU())
+            for i in range(len(hidden_layers)-1):
+                linear_layers.append(nn.Linear(hidden_layers[i], hidden_layers[i+1]))
+                linear_layers.append(nn.ReLU())
+            linear_layers.append(nn.Linear(hidden_layers[-1], 1))
+        else:
+            linear_layers.append(nn.Linear(math.prod(cnn_output),1))
+        self.linear_stack = nn.Sequential(*linear_layers)
+
+    def forward(self,X:Tensor)->Tensor:
+        """
+        Predicts energy for the wavefunction
+        :param X: Potential image
+        :return: Energy prediction
+        """
+        X = self.linear_stack(self.cnn_stack(X))
+        return X
