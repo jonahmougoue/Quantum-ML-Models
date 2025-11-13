f3e1039c54990aa214274ec0a717ec3d3742940e
Jonah <jonah.mougoue@gmail.com>
2025-11-12 19:23:12 -0500

    Update docstrings

--- HelperFunctions/plot_loss.py
+++ HelperFunctions/plot_loss.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 def plot_loss(loss1:list,epochs:int,loss2:list=None,label1:str=None,label2:str=None)->None:
-    '''
+    """
     Function for plotting losses
     :param loss1: List of loss per epoch
     :param epochs: Number of cycles
@@ -8,7 +8,7 @@ def plot_loss(loss1:list,epochs:int,loss2:list=None,label1:str=None,label2:str=N
     :param label1: Label for loss1
     :param label2: Label for loss2
     :return: None
-    '''
+    """
     plt.plot(range(1,epochs+1),loss1,label=label1)
     if loss2 is not None:
         plt.plot(range(1,epochs+1),loss2,label=label2)
