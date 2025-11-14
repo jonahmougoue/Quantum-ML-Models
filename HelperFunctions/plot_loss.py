1fe0a956f31ab9e24d6cf6f7969caa7ccdb2956f
Jonah <jonah.mougoue@gmail.com>
2025-11-14 18:16:09 -0500

    Remove redundant epochs argument

--- HelperFunctions/plot_loss.py
+++ HelperFunctions/plot_loss.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
-def plot_loss(loss1:list,epochs:int,loss2:list=None,label1:str=None,label2:str=None)->None:
+def plot_loss(loss1:list,loss2:list=None,label1:str=None,label2:str=None)->None:
     """
     Function for plotting losses
     :param loss1: List of loss per epoch
-    :param epochs: Number of cycles
     :param loss2: List of loss per epoch
     :param label1: Label for loss1
     :param label2: Label for loss2
     :return: None
     """
-    plt.plot(range(1,epochs+1),loss1,label=label1)
+    plt.plot(range(1,len(loss1)+1),loss1,label=label1)
     if loss2 is not None:
-        plt.plot(range(1,epochs+1),loss2,label=label2)
+        plt.plot(range(1,len(loss2)+1),loss2,label=label2)
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.yscale('log')
