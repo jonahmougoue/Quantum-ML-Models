c578729b5f2262bdf07690da2cb301bc9707e023
Jonah <jonah.mougoue@gmail.com>
2025-11-15 18:40:31 -0500

    Add energy accuracy plots per potential type

--- HelperFunctions/plot_energy.py
+++ HelperFunctions/plot_energy.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import r2_score
+
 def plot_energy(E_pred,E_true,file_labels,potential_labels):
 
     unique_labels = sorted(set(potential_labels))
@@ -7,14 +9,35 @@ def plot_energy(E_pred,E_true,file_labels,potential_labels):
     E_pred = np.array(E_pred)
     V_labels = np.array(potential_labels)
 
-    for label in unique_labels:
-        mask = V_labels == label
-        plt.scatter(E_true[mask], E_pred[mask], label=file_labels[label])
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
-    line = np.linspace(0, max(max(E_true), max(E_pred)),2)
-    plt.plot(line,line,label='y=x')
-    plt.xlabel('True Energy (Ha)')
-    plt.ylabel('Predicted Energy (Ha)')
-    plt.title('Energy Accuracy')
-    plt.legend()
+    fig, axes = plt.subplots(3, 2, figsize=(18, 27))
+    axes = axes.flatten()  # flatten to 1D list of axes
+
+    line = np.linspace(0, max(max(E_true), max(E_pred)), 2)
+    axes[0].plot(line, line, label='y=x',color='black')
+    for i, label in enumerate(unique_labels):
+        mask = V_labels == label
+        axes[0].scatter(E_true[mask], E_pred[mask], label=file_labels[label],color=colors[i])
+    r2 = r2_score(E_true, E_pred)
+    axes[0].set_xlabel('True Energy (Ha)')
+    axes[0].set_ylabel('Predicted Energy (Ha)')
+    axes[0].set_title('Total Energy Accuracy')
+    plot_text = rf'$R^2 = {r2:.4f}$'+f'\nMedian Absolute Error: {np.median(np.absolute(E_true - E_pred))*10**3:.2f} mHa'
+    axes[0].text(0.95,0.05,plot_text,transform=axes[0].transAxes,ha='right',va='bottom')
+    axes[0].legend(loc='upper left')
+
+    for i, label in enumerate(unique_labels):
+        mask = V_labels == label
+        line = np.linspace(0, max(max(E_true[mask]), max(E_pred[mask])),2)
+        axes[i+1].plot(line,line, label='y=x',color='black')
+        axes[i+1].scatter(E_true[mask], E_pred[mask],label=file_labels[label],color=colors[i])
+        r2 = r2_score(E_true[mask],E_pred[mask])
+        axes[i+1].set_xlabel('True Energy (Ha)')
+        axes[i+1].set_ylabel('Predicted Energy (Ha)')
+        axes[i+1].set_title(rf'{file_labels[label]} Energy Accuracy')
+        plot_text = rf'$R^2 = {r2:.4f}$'+f'\nMedian Absolute Error: {np.median(np.absolute(E_true[mask] - E_pred[mask]))*10**3:.2f} mHa'
+        axes[i+1].text(0.95,0.05,plot_text,transform=axes[i+1].transAxes,ha='right',va='bottom')
+        axes[i+1].legend(loc='upper left')
     plt.show()
+
